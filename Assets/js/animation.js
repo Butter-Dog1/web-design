2a834d8ea394732451437ff62f187eede62b8681
TacoMarco <tacoomarcoo@gmail.com>
2022-02-18 23:40:29 +1100

    Added typing animation for index.html

--- Assets/js/animation.js
+++ Assets/js/animation.js
@@ -1,1 +1,27 @@
-console.log('test')
+function sleep(milliseconds) {  
+    return new Promise(resolve => setTimeout(resolve, milliseconds));  
+}  
+
+async function animate() {
+    var word = ["Z", "a", "c", "s", " ", "w", "e", "b", "s", "i", "t", "e"];
+    
+    var title = document.getElementById("title")
+    var change = true
+    for (let index = 0; index < word.length; index++) {
+        title.append(word[index])
+        await sleep(100)
+
+        if (index == 3 && change) {
+            await sleep(500)
+            title.innerHTML = "Zac"
+            await sleep(500)
+            title.append("'")
+            index = 2
+            change = false
+            await sleep(200)
+        }
+    }
+    console.log(word[1-2])
+}
+
+window.onload = animate()
